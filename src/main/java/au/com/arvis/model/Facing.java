73e8d217bddc69cb49bf7e694fc9cb7455d7c5aa
Jack Wang <jack.wang@surefiresystems.com>
2020-02-18 14:05:30 +1100

    Updated readme with instructions. Removed commented out code.

--- src/main/java/au/com/arvis/model/Facing.java
+++ src/main/java/au/com/arvis/model/Facing.java
@@ -4,41 +4,4 @@ package au.com.arvis.model;
 public enum Facing {
 
     NORTH, EAST, SOUTH, WEST
-
-//   // Facing(int value){
-//       // this.value = value;
-//   // }
-//
-//   // private int value;
-//
-//   // public int getValue() {
-//        return value;
-//    }
-//
-//    public static Facing getFacing(int value){
-//
-//        if(value < 0){
-//
-//            value = value + 360;
-//        }
-//
-//        switch (value){
-//
-//            case 0:
-//                return NORTH;
-//
-//            case 90:
-//                return EAST;
-//
-//            case 180:
-//                return SOUTH;
-//
-//            case 270:
-//                return WEST;
-//
-//            default:
-//                return NORTH;
-//
-//        }
-//    }
 }
